3a8c76a8071b97bb77adf7df44fe78ab0a192c36
Noldenval Santana <noldenval.dev@gmail.com>
2025-09-24 11:24:15 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,14 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYQm0K_4EdKHvvXOs81raKmstmAsRdT6g",
+  apiKey: "AIzaSyCYQm0K_4EdKHvvXOs81raKmstmAsRdT6g", // ← VERIFIQUE SE ESTÁ CORRETA
   authDomain: "controlemeubolso.firebaseapp.com",
   databaseURL: "https://controlemeubolso-default-rtdb.firebaseio.com",
   projectId: "controlemeubolso",
   storageBucket: "controlemeubolso.firebasestorage.app",
   messagingSenderId: "369393280169",
-  appId: "1:369393280169:web:0b0d768f074e0ff4c775b8",
-  measurementId: "G-0EYHJF137F"
+  appId: "1:369393280169:web:0b0d768f074e0ff4c775b8"
 };
 
 const app = initializeApp(firebaseConfig);

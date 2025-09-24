618bb1c780a0b84a2f648d8e3f7f5e7c3aec8fb7
Noldenval Santana <noldenval.dev@gmail.com>
2025-09-24 10:47:50 -0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,19 @@
+// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYQm0K_4EdKHvvXOs81raKmstmAsRdT6g",
+  authDomain: "controlemeubolso.firebaseapp.com",
+  databaseURL: "https://controlemeubolso-default-rtdb.firebaseio.com",
+  projectId: "controlemeubolso",
+  storageBucket: "controlemeubolso.firebasestorage.app",
+  messagingSenderId: "369393280169",
+  appId: "1:369393280169:web:0b0d768f074e0ff4c775b8",
+  measurementId: "G-0EYHJF137F"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
